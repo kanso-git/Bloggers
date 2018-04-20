30003843c611d9c53c8ca955ae4f9237dd7c93bc
Abdallah Kanso <abdallah.kanso@gmail.com>
2018-04-20 23:21:48 +0200

    enhance caching logic part2

--- services/cache.js
+++ services/cache.js
@@ -4,12 +4,13 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379'
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function(){
+mongoose.Query.prototype.cache = function(options = {}){
     this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
     return this;
 }
 
@@ -27,7 +28,7 @@ mongoose.Query.prototype.exec = async function() {
     collection:this.mongooseCollection.name
    }));
   // See if we have a value for 'key' in redis  
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   // If we do, return that
   if(cacheValue){
@@ -40,7 +41,7 @@ mongoose.Query.prototype.exec = async function() {
   const result = await exec.apply(this, arguments);
   
   // set the expiration time to 10 ms 
-  client.set(key,JSON.stringify(result),'EX', 10);
+  client.hset(this.hashKey, key, JSON.stringify(result),'EX', 10);
 
   return result;
 };
