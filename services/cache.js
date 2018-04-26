fad4c77f48c33b02a584c003becb588a76aaf531
kanso <abdallah.kanso@gmail.com>
2018-04-26 12:24:03 +0200

    add ci integration

--- services/cache.js
+++ services/cache.js
@@ -1,56 +1,52 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
+const keys = require('../config/keys');
 
-const redisUrl = 'redis://127.0.0.1:6379'
-const client = redis.createClient(redisUrl);
+const client = redis.createClient(keys.redisUrl);
 client.hget = util.promisify(client.hget);
-
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function(options = {}){
-    this.useCache = true;
-    this.hashKey = JSON.stringify(options.key || '');
-    return this;
-}
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
+
+  return this;
+};
 
 mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
 
-  console.log(` ${this.mongooseCollection.name} useCache:${this.useCache || false}`);
- if(!this.useCache){
-    return exec.apply(this,arguments);
- }
-    // this = Query
- // console.log(this.mongooseCollection.name);
- // console.log(this.getQuery());
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
 
- const key = JSON.stringify(Object.assign({}, this.getQuery(),{
-    collection:this.mongooseCollection.name
-   }));
-  // See if we have a value for 'key' in redis  
+  // See if we have a value for 'key' in redis
   const cacheValue = await client.hget(this.hashKey, key);
 
   // If we do, return that
-  if(cacheValue){
-    const doc = JSON.parse(cacheValue)
-   return  Array.isArray(doc)
-    ? doc.map(d => new this.model(d) )
-    : new this.model(doc);
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
+
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
   }
+
   // Otherwise, issue the query and store the result in redis
   const result = await exec.apply(this, arguments);
-  
-  // set the expiration time to 10 ms 
-  client.hset(this.hashKey, key, JSON.stringify(result),'EX', 10);
+
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
   return result;
 };
 
-function clearHash(hashKey){
-  console.log(`clear cache for ${hashKey}`);
-  client.del(JSON.stringify(hashKey))
-}
-
-module.exports ={
-  clearHash
-}
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
+};
--- tests/header.test.js
+++ tests/header.test.js
@@ -1,31 +1,34 @@
-
 const Page = require('./helpers/page');
+
 let page;
 
-beforeEach( async ()=>{
-    page = await Page.build();
-    await page.goto('localhost:3000');
-})
-
-test('The header text is equal to Blogster ', async ()=>{
-  // Get the "viewport" of the page, as reported by the page.
-  const brandLogo = await page.$eval('a.brand-logo', el => el.innerHTML);
-  console.log(`brandLogo is:${brandLogo}`);
-  expect(brandLogo).toEqual('Blogster');
+beforeEach(async () => {
+  page = await Page.build();
+  await page.goto('http://localhost:3000');
 });
 
-test('Clicking login starts oauth flow', async ()=>{
- await page.click('.right a');
- const url =await page.url();
- expect(url).toMatch(/accounts\.google\.com/);
+afterEach(async () => {
+  await page.close();
 });
 
-test('When signed in, shows logout button', async () =>{
-    await page.login();
-    await page.waitFor('a[href="/auth/logout"]');
-    const text = await page.getContentOf('a[href="/auth/logout');
-    expect(text).toEqual('Logout');
- });
-afterEach(async ()=>{
-    await page.close();
-})
+test('the header has the correct text', async () => {
+  const text = await page.getContentsOf('a.brand-logo');
+
+  expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+  await page.login();
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual('Logout');
+});
